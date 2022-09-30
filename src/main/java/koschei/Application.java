785acbf7c072b5178e785a9f6518becd583b30a8
xidef-fedix <109800756+xidef-fedix@users.noreply.github.com>
2022-09-30 11:47:20 +0300

    first commit

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -1,6 +1,7 @@
 package koschei;
 
 import koschei.config.AppConfig;
+import koschei.models.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,6 +11,15 @@ public class Application {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+        Ocean1 ocean1 = applicationContext.getBean(Ocean1.class);
+        Island2 island2 = applicationContext.getBean(Island2.class);
+        Wood3 wood3 = applicationContext.getBean(Wood3.class);
+        Rabbit4 rabbit4 = applicationContext.getBean(Rabbit4.class);
+        Duck5 duck5 = applicationContext.getBean(Duck5.class);
+        Egg6 egg6 = applicationContext.getBean(Egg6.class);
+        Needle7 needle7 = applicationContext.getBean(Needle7.class);
+        Deth8 deth8 = applicationContext.getBean(Deth8.class);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
